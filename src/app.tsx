b3bae7a4f2f8758e51f2cb07386c2c05cd8b374d
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-24 13:48:28 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <p className="font-mono font-bold text-xl">Hello</p>
+    <div className="w-screen h-screen flex flex-col md:flex-row items-center justify-center gap-4 bg-zinc-100 font-mono p-4">
+      <textarea
+        spellCheck={false}
+        defaultValue="console.log('Hello World');"
+        className="border border-zinc-300 rounded-md shadow-sm outline-none w-full h-full p-4 resize-none"
+      />
+      <div className="w-full h-full bg-black text-white rounded-md shadow-sm p-4">
+        <p>Output</p>
+      </div>
     </div>
   );
 }
