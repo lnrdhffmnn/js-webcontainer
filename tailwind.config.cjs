ca23b6851be9fd2c8bf5495fe7d29487930f4f2a
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-24 18:33:43 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        screen: ["100vw", "100svw"],
+      },
+      height: {
+        screen: ["100vh", "100svh"],
+      },
+    },
   },
   plugins: [],
 };
